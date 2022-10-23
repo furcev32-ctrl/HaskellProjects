affd7abcd9993aaef1cbe957c89eb202c9344a45
Furkan MHP Mac <tasafu31@gmail.com>
2022-10-23 19:02:52 +0200

    Some tries for gizzBuzz

--- P1/App.hs
+++ P1/App.hs
@@ -1,9 +1,22 @@
 -- To RUN the code use the command: "ghc --make App.hs"
 -- then type ./App
 
-doubleIt :: Int -> Int
-doubleIt x = x * 2
+isDivBy3 :: Int -> Bool
+isDivBy3 x = ((mod x 3) == 0)
+
+isDivBy5 :: Int -> Bool
+isDivBy5 x = ((mod x 5) == 0)
+
+fizzBuzzConverter :: [Char] -> [Char]
+fizzBuzzConverter x = replaceWithFizzBuzz (x ++ ((if (isDivBy3 x) then "Fizz" else "")  ++ (if (isDivBy5 x) then "Buzz" else "")))
+
+replaceWithFizzBuzz :: [Char] -> [Char]
+returnStringIfNotEmpty x:xs = if xs == "" then x else xs
+
+putFizzBuzz :: [Int] -> [[Char]]
+putFizzBuzz x = map fizzBuzzConverter ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 main :: IO()
 main = do
-    print(doubleIt 200)
+    
+    -- putFizzBuzz [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
