eedbb4f126ccb15b66bedd2e1917994b55ca2632
Furkan MHP Mac <tasafu31@gmail.com>
2022-11-09 22:46:51 +0100

    Finisched UB2.2

--- UB2/Polynom.hs
+++ UB2/Polynom.hs
@@ -1,8 +1,19 @@
 module Polynom where
 
---cmult :: Polynom -> Double -> Polynom
-cmult p x = map (x*) p
+type Polynom = [Double]
 
---eval :: Polynom -> Double -> Double
-eval p x = foldr (\e n-> e + x*n) 0 p
+cmult :: Polynom -> Double -> Polynom
+cmult [] x = []
+cmult poly 0 = []
+cmult poly 1 = poly
+cmult poly x = map (x*) poly
 
+eval :: Polynom -> Double -> Double
+eval [] x = 0
+eval (p:ps) 0 = p
+eval poly 1 = sum poly
+eval poly x = foldr (\e n-> e + x*n) 0 poly
+
+deriv :: Polynom -> Polynom
+deriv [] = []
+deriv (p:ps) =  zipWith (*) [1..] ps
