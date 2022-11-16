48277df64292c029737d3dbb9f6a596e3aa479be
Furkan MHP Mac <tasafu31@gmail.com>
2022-11-16 18:40:38 +0100

    UB3.1 - .2 Finished.

---
+++ UB3/Collatz.hs
@@ -0,0 +1,16 @@
+module Fibs where
+
+collatz :: Int -> [Int]
+collatz = iterate (\n -> if n `mod` 2 == 0 then n `div` 2 else 3*n + 1 )
+    
+
+num :: Int -> Int
+num m = getIndexOfTheFirstOne (collatz m) 0
+
+getIndexOfTheFirstOne (x:xs) acc
+    | x == 1 = acc
+    | otherwise = getIndexOfTheFirstOne xs (acc + 1)
+
+maxNum :: Int -> Int -> (Int, Int)
+maxNum a b = foldr (\x y -> if (snd x) >= (snd y) then x else y ) (a, num a) (map (\m -> (m ,num m)) [a..b])
+
---
+++ UB3/Fibs.hs
@@ -0,0 +1,6 @@
+module Fibs where
+
+
+-- fibs :: [Integer]
+fibs = [0,1] ++ (zipWith (+) (fibs) (tail fibs))
+
